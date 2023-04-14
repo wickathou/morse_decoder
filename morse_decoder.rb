9e93739ef696fe1f92cea1931902b32ba59fc5c4
Ayo Moses <ayomoses111@gmail.com>
2023-04-14 14:53:12 +0100

    resolved linter errors

--- morse_decoder.rb
+++ morse_decoder.rb
@@ -1,55 +1,54 @@
-MORSE = Hash.new
+MORSE = {}
 MORSE = {
-  '.-'=>'A',
-  '-...'=>'B',
-  '-.-.'=>'C',
-  '-..'=>'D',
-  '.'=>'E',
-  '..-.'=>'F',
-  '--.'=>'G',
-  '....'=>'H',
-  '..'=>'I',
-  '.---'=>'J',
-  '-.-'=>'K',
-  '.-..'=>'L',
-  '--'=>'M',
-  '-.'=>'N',
-  '---'=>'O',
-  '.--.'=>'P',
-  '--.-'=>'Q',
-  '.-.'=>'R',
-  '...'=>'S',
-  '-'=>'T',
-  '..-'=>'U',
-  '...-'=>'V',
-  '.--'=>'W',
-  '-..-'=>'X',
-  '-.--'=>'Y',
-  '--..'=>'Z',
-  '-----'=>'0',
-  '.----'=>'1',
-  '..---'=>'2',
-  '...--'=>'3',
-  '....-'=>'4',
-  '.....'=>'5',
-  '-....'=>'6',
-  '--...'=>'7',
-  '---..'=>'8',
-  '----.'=>'9'
+  '.-' => 'A',
+  '-...' => 'B',
+  '-.-.' => 'C',
+  '-..' => 'D',
+  '.' => 'E',
+  '..-.' => 'F',
+  '--.' => 'G',
+  '....' => 'H',
+  '..' => 'I',
+  '.---' => 'J',
+  '-.-' => 'K',
+  '.-..' => 'L',
+  '--' => 'M',
+  '-.' => 'N',
+  '---' => 'O',
+  '.--.' => 'P',
+  '--.-' => 'Q',
+  '.-.' => 'R',
+  '...' => 'S',
+  '-' => 'T',
+  '..-' => 'U',
+  '...-' => 'V',
+  '.--' => 'W',
+  '-..-' => 'X',
+  '-.--' => 'Y',
+  '--..' => 'Z',
+  '-----' => '0',
+  '.----' => '1',
+  '..---' => '2',
+  '...--' => '3',
+  '....-' => '4',
+  '.....' => '5',
+  '-....' => '6',
+  '--...' => '7',
+  '---..' => '8',
+  '----.' => '9'
 }
 
-def decodeChar (randomChar)
+def decodeChar(randomChar)
   MORSE[randomChar]
 end
 
-def decodeWord (randomChar)
-  return randomChar.split(" ").map { |string| decodeChar string }.join
+def decodeWord(randomChar)
+  randomChar.split(' ').map { |string| decodeChar string }.join
 end
 
-def decodeSentence (randomChar)
-  words = randomChar.split("   ")
-  puts words.map {|word| decodeWord word}.join(" ")
-  
+def decodeSentence(randomChar)
+  words = randomChar.split('   ')
+  puts words.map { |word| decodeWord word }.join(' ')
 end
 
 decodeSentence ' .-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...'
